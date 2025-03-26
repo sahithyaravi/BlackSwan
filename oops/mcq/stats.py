8571e5da4804e88ca872313604a0b752ed4261fa
Aditya Chinchure <aditya.chinchure@gmail.com>
2025-03-25 22:13:16 -0700

    Cleanup

--- oops/mcq/stats.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-with open('mcq_list_v2_gpt.json') as f:
-    data = json.load(f)
-
-print("Length of data: ", len(data))
-
-print("GPT-modded", len([d for d in data if d['gpt_modified'] == 'yes']))
